7d46a7bc5fff480aa435bac1b2652b189bff284c
saboalfazl6162 <saboalfazl6162@gmail.com>
2025-11-06 07:14:26 +0330

    debug user model and model admin

--- apps/authenticated/admin.py
+++ apps/authenticated/admin.py
@@ -8,21 +8,26 @@ from django.utils.translation import gettext_lazy as _
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
+    list_display = ('username','__str__','email','bale_id','is_active','is_staff','is_superuser')
+    list_editable = ('is_active','is_staff','is_superuser')
+    list_display_links = ('username','__str__')
     readonly_fields = ("date_joined", "last_login")
+    
     add_fieldsets = (
-        (None, {
-            'classes':('wide',),
-            'fields': (
-                "username","email","password1","password2",
-            ),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username","password1","password2","email","bale_id"),
+            },
+        ),
     )
     fieldsets = (
         (_('important fields'), {
             'classes':('wide'),
             'fields': (
-                "username","email","password1","password2",
-                "first_name","last_name",
+                "username","email","password",
+                "first_name","last_name","bale_id"
             ),
         }),
         (_('advanced fields'), {
--- apps/authenticated/models.py
+++ apps/authenticated/models.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     email = models.EmailField(verbose_name=_("email"),unique=True)
     bale_id = models.CharField(max_length=110,verbose_name="شناسه بله",null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
